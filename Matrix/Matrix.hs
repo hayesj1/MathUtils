77510c12bdda04c5adf31a517e855a0aad1bb073
Jacob Hayes <tigerbird1@verizon.net>
2016-10-18 14:37:15 -0400

    Fully implemented Matrix.hs, added Utility funcs
    Matrix.hs fully fleshed out. Added some Utility functions to Util.hs

--- Matrix/Matrix.hs
+++ Matrix/Matrix.hs
@@ -1,185 +1,247 @@
--- ***Matrix & Related DataTypes***
+-- ***Matrix***
 module Matrix.Matrix
 ( 
 {- DataTypes -}
-  Vector (EmptyVec, ZeroVec)
-, Matrix (EmptyMat, ZeroMat)
+  Matrix ()
+{- Factory Functions -}
+, makeMatrixFiller
+, makeMatrixLinkedList
+, makeIdentityMatrix
+, makeZeroMatrix
+, listToMatrix
+, matrixToList
+{- Methods -}
+, scale
+, mAdd
+, mSub
+, transpose
+, trace
+, determinant
+, coFactor
+, numRows
+, numCols
+, size
+, isSquare
+, scaleRow
+, sumRow
 {- Misc Functions -}
-, numComponents
+, getMaybeRow
+, getRightMatrix
 ) where
 
+
+--- **Imports**
+import qualified Data.List as LST -- for transpose
+
 import qualified Matrix.Util as Util
-
---- **DataType Declearations**
-
----- Vector:
-----   EmptyVec -- 0-D Vector for intenral use
-----   ZeroVec  -- N-D Vector of zeros
-----   Vec1D    -- 1-D Vector of a Double
-----   Vec2D    -- 2-D Vector of Doubles
-----   Vec3D    -- 3-D Vector of Doubles
-----   VecND    -- N-D Vector of Doubles
-data Vector = EmptyVec
-              | ZeroVec { v :: Vector }
-              | Vec1D { v1 :: Double }
-              | Vec2D { v1 :: Double
-                      , v2 :: Double
-                      }
-              | Vec3D { v1 :: Double
-                      , v2 :: Double
-                      , v3 :: Double
-                      }
-              | VecND { h :: Double
-                      , t :: Vector
-                      }
-                      deriving (Eq, Ord, Show, Read)
-
----- Matrix:
-----   EmptyMat -- 0x0 Matrix for intenral use
-----   IdenMat  -- NxN Identity Matrix
-----   ZeroMat  -- NxN Matrix of zeros
-----   Mat1x1   -- 1x1 Matrix of a Double
-----   Mat2x2   -- 2x2 Matrix of Doubles
-----   Mat3x3   -- 3x3 Matrix of Doubles
-----   Mat1xN   -- 1x1 Matrix of Doubles
-----   Mat2XN   -- 2xN Matrix of Doubles
-----   Mat3XN   -- 3xN Matrix of Doubles
-----   MatNXN   -- NxN Matrix of Doubles
-data Matrix = EmptyMat
-            | ZeroMat { mat :: [Vector] }
-            | IdenMat { matIden :: [Vector] }
-            | Mat1x1  { r1 :: Vector }
-            | Mat2x2  { r1 :: Vector
-                      , r2 :: Vector
-                      }
-            | Mat3x3  { r1 :: Vector
-                      , r2 :: Vector
-                      , r3 :: Vector
-                      }
-            | Mat1xN  { r1 :: Vector }
-            | Mat2xN  { r1 :: Vector
-                      , r2 :: Vector
-                      }
-            | Mat3xN  { r1 :: Vector
-                      , r2 :: Vector
-                      , r3 :: Vector
-                      }
-            | MatNxM { mat :: [Vector] }
-                     deriving (Eq, Ord, Show, Read)
-
---- **DataType Creation Functions**
-
---- *Vector*
-
----- Returns an empty Vector
-makeEmptyVec:: Vector
-makeEmptyVec = EmptyVec
-
----- Returns a ZeroVec of N dimension
-makeZeroVec:: Int -> Vector
-makeZeroVec n = (ZeroVec { v=(makeVecNDInternal n)})
-          where makeVecNDInternal:: Int -> Vector
-                makeVecNDInternal n
-                                | n <= 1 = EmptyVec
-                                | otherwise = (VecND { h=0, t=(getValOrErr (makeVecND (replicate (n-1) 0) (n-1))) })
-
----- Returns a Vec1D
-makeVec1D:: Double -> Vector
-makeVec1D x = (Vec1D { v1=x })
-
----- Returns a Vec2D
-makeVec2D:: (Double, Double) -> Vector
-makeVec2D (x, y) = (Vec2D { v1=x, v2=y })
-
----- Returns a Vec3D
-makeVec3D:: (Double, Double, Double) -> Vector
-makeVec3D (x, y, z) = (Vec3D { v1=x, v2=y, v3=z })
-
----- Returns a VecND
-makeVecND:: [Double] -> Int -> Either String Vector
-makeVecND l@(vi:v) n
-                | null l || n <= 0 = Left $ "Vectors must have dimension >= 1"
-                | null v || n == 1 = Right (VecND { h=vi, t=(makeEmptyVec) })
-                | otherwise = Right (VecND { h=vi, t=(getValOrErr (makeVecND v (n-1))) })
+import Matrix.LinkedList
 
 
---- *Matrix*
+--- **Type Synomyms**
 
----- Returns an empty Matrix
-makeEmptyMat:: Matrix
-makeEmptyMat = EmptyMat
+---- Treat LinkedLists or Doubles as Rows
+type Row = (LinkedList Double)
 
----- Returns a ZeroMat of RxC dimension
-makeZeroMat:: Int -> Int -> Either String Matrix
-makeZeroMat 0 _ = Left $ "A matrix must have at least 1 row"
-makeZeroMat _ 0 = Left $ "A matrix must have at least 1 column"
-makeZeroMat r c = Right (ZeroMat { mat=(replicate r (makeZeroVec c)) })
+---- Treat (Int,Int) pairs are Dimensions for Matricies
+type Dim = (Int, Int)
 
----- Returns a Mat1x1
-makeMat1x1:: Vector -> Either String Matrix
-makeMat1x1 v1@(Vec1D _) = Right (Mat1x1 { r1=v1 })
-makeMat1x1 v1 = Left $ "Vector " ++ (show v1) ++ " does not have the same dimension as a row for a: " ++ "1x1 Matrix"
 
----- Returns a Mat2x2
-makeMat2x2:: (Vector, Vector) -> Either String Matrix
-makeMat2x2 (v1@(Vec2D _ _), v2@(Vec2D _ _)) = Right (Mat2x2 { r1=v1, r2=v2 })
-makeMat2x2 (v1, v2) = Left $ "Vectors " ++ (show v1) ++ " and " ++ (show v2) ++ " do not have the same dimension as a row for a: " ++ "2x2 Matrix"
+--- **DataType Declarations**
 
----- Returns a Mat3x3
-makeMat3x3:: (Vector, Vector, Vector) -> Either String Matrix
-makeMat3x3 (v1@(Vec3D _ _ _), v2@(Vec3D _ _ _), v3@(Vec3D _ _ _)) = Right (Mat3x3 { r1=v1, r2=v2, r3=v3 })
-makeMat3x3 (v1, v2, v3) = Left $ "Vectors " ++ (show v1) ++ ", " ++ (show v2) ++ " and " ++ (show v3) ++ " do not have the same dimension as a row for a: " ++ "3x3 Matrix"
+---- Matrix: a Matrix of Doubles
+data Matrix = Matrix (LinkedList Row) deriving (Eq, Ord, Show, Read)
 
----- Returns a Mat1xN
-makeMat1xN:: Int -> Vector -> Either String Matrix
-makeMat1xN cols v1@(VecND _ _) 
-            | (numComponents v1) == cols = Right (Mat1xN { r1=v1 })
-            | otherwise = Left $ "Vector " ++ (show v1) ++ "does not have " ++ (show cols) ++ " components, required for a:" ++ "1xN Matrix, with N=" ++ (show cols)
-makeMat1xN cols v1 = Left $ "Vector " ++ (show v1) ++ " is not of type VecND, as required for a: " ++ "1xN Matrix, with N=" ++ (show cols)
 
----- Returns a Mat2xN
-makeMat2xN:: Int -> (Vector, Vector) -> Either String Matrix
-makeMat2xN cols (v1@(VecND _ _), v2@(VecND _ _))
-            | ((numComponents v1) == (numComponents v2)) && ((numComponents v1) == cols) = Right (Mat2xN { r1=v1, r2=v2 })
-            | otherwise = Left $ "Vectors " ++ (show v1) ++ " and " ++ (show v2) ++ "do not have " ++ (show cols) ++ " components, required for a:" ++ "2xN Matrix, with N=" ++ (show cols)
-makeMat2xN cols (v1,  v2) = Left $ "Vectors " ++ (show v1) ++ " and " ++ (show v2) ++ " are not of type VecND, as required for a: " ++ "2xN Matrix, with N=" ++ (show cols)
+--- **DataType Factory Functions**
 
----- Returns a Mat3xN
-makeMat3xN:: Int -> (Vector, Vector, Vector) -> Either String Matrix
-makeMat3xN cols (v1@(VecND _ _), v2@(VecND _ _), v3@(VecND _ _))
-            | ((numComponents v1) == (numComponents v2)) && ((numComponents v2) == (numComponents v3)) && ((numComponents v1) == cols) = Right (Mat3xN { r1=v1, r2=v2, r3=v3 })
-            | otherwise = Left $ "Vectors " ++ (show v1) ++ ", " ++ (show v2) ++ " and " ++ (show v3) ++ "do not have " ++ (show cols) ++ " components, required for a:" ++ "3xN Matrix, with N=" ++ (show cols)
-makeMat3xN cols (v1, v2, v3) = Left $ "Vectors " ++ (show v1) ++ ", " ++ (show v2) ++ " and " ++ (show v3) ++ " are not of type VecND, as required for a: " ++ "3xN Matrix, with N=" ++ (show cols)
+---- Creates a Matrix, with param 1 as (rows, cols), and param 2 as the default value
+----   Returns an error String instead if either rows or cols is <= 0
+makeMatrixFiller:: (Int, Int) -> Double -> Either String Matrix
+makeMatrixFiller (0,_) _ = Left $ getErrMsg 10 "makeMatrixFiller"
+makeMatrixFiller (_,0) _ = Left $ getErrMsg 10 "makeMatrixFiller"
+makeMatrixFiller dim val = Right (Matrix (makeMatrixFillerInternal dim val))
+            where makeMatrixFillerInternal:: (Int, Int) -> Double -> (LinkedList Row)
+                  makeMatrixFillerInternal (0,_) _ = EmptyList
+                  makeMatrixFillerInternal (_,0) _ = EmptyList
+                  makeMatrixFillerInternal (r,c) val = Node (makeLinkedListFiller c val) (makeMatrixFillerInternal (r-1,c) val)
 
----- Returns a MatNxM of RxC dimension
-makeMatNxM:: Int -> Int -> [[Double]] -> Either String (Either Vector Matrix)
-makeMatNxM 0 _ _ = Left $ "A matrix must have at least 1 row"
-makeMatNxM _ 0 _ = Left $ "A matrix must have at least 1 column"
-makeMatNxM r c [] = Right $ Right (MatNxM { mat=(replicate r (getValOrErr (makeVecND (replicate c 0) (c-1)))) })
-makeMatNxM r c (h:t)
-                | null t = Right $ Left (getValOrErr (makeVecND h c))
-                | otherwise = Right $ Right (MatNxM { mat=((getValOrErr (makeVecND h c)) : [getLeft (getRightLROrErr (makeMatNxM (r-1) c t))]) })
-                where getRightLROrErr:: Either String (Either Vector Matrix) -> (Either Vector Matrix)
-                      getRightLROrErr (Right (Left rl)) = Left rl
-                      getRightLROrErr (Right (Right rr)) = Right rr
-                      getRightLROrErr (Left l)= Util.err l
-                      getLeft:: Either Vector Matrix -> Vector
-                      getLeft (Left l) = l
-                      getRight:: Either Vector Matrix -> Matrix
-                      getRight (Right r) = r
+---- Converts a LinkedList of LinkedLists into a Matrix
+----   Returns an error String instead if param 1 is an EmptyList
+makeMatrixLinkedList:: (LinkedList Row) -> Either String Matrix
+makeMatrixLinkedList (EmptyList) = Left $ getErrMsg 10 "makeMatrixLinkedList"
+makeMatrixLinkedList llist = if checkJagged mat then Left (getErrMsg 11 "makeMatrixLinkedList") else Right mat
+            where mat = Matrix llist
+
+---- Constructs the Identity Matrix of dimension (param 1) by (param 1)
+----   Returns an error String instead if param 1 is <= 0
+makeIdentityMatrix:: Int -> Either String Matrix
+makeIdentityMatrix size
+            | size <= 0 = Left $ getErrMsg 10 "makeIdentityMatrix"
+            | otherwise = (listToMatrix (makeIdentityList size (size-1)))
+            where makeIdentityList:: Int -> Int -> [[Double]]
+                  makeIdentityList size row
+                            | size <= 0 || row < 0 = [[]]
+                            | row == 0 =  [(fromIntegral 1) : [ (fromIntegral (x-x)) | x <- [0..(size-2)] ]]
+                            | otherwise = (makeIdentityList size (row-1)) ++ [[ y | x <- [0..(size-1)], let y = (if x /= row then (fromIntegral 0) else (fromIntegral 1)) ]]
+
+---- Shortcut for: makeMatrixFiller (param 1) 0
+----   See makeMatrixFiller
+makeZeroMatrix:: (Int,Int) -> Either String Matrix
+makeZeroMatrix dim = (makeMatrixFiller dim 0)
+
+---- Converts a List of Lists of Doubles into a Matrix
+----   Returns an error String instead if param 1 is [] or [[]]
+listToMatrix:: [[Double]] -> Either String Matrix
+listToMatrix [] = Left $ getErrMsg 10 "listToMatrix"
+listToMatrix [[]] = Left $ getErrMsg 10 "listToMatrix"
+listToMatrix list = if checkJagged mat then Left (getErrMsg 11 "listToMatrix") else Right mat
+            where mat = Matrix (listToMatrixInternal list)
+                  listToMatrixInternal:: [[Double]] -> (LinkedList Row)
+                  listToMatrixInternal [] = EmptyList
+                  listToMatrixInternal (h:t) = Node (listToLinkedList h) (listToMatrixInternal t)
+
+---- Converts a matrix to a list of lists
+matrixToList:: Matrix -> [[Double]]
+matrixToList (Matrix  m) = matrixToListInternal m
+            where matrixToListInternal:: (LinkedList Row) -> [[Double]]
+                  matrixToListInternal (EmptyList) = []
+                  matrixToListInternal m@(Node d t) = (linkedListToList d) : (matrixToListInternal t)
+
+
+--- **DataType Methods**
+
+---- Scales a Matrix: (x)*m
+scale:: Matrix -> Double -> Matrix
+scale (Matrix (EmptyList)) _ = Matrix $ EmptyList
+scale (Matrix m) x = Matrix $ scaleInternal x m
+            where scaleInternal:: Double -> (LinkedList Row) -> (LinkedList Row)
+                  scaleInternal x (Node r (EmptyList)) = makeNode (scaleRow r x)
+                  scaleInternal x m@(Node r rs) = makeNodeT (scaleRow r x) (scaleInternal x rs)
+
+---- Adds two matrices: m1 + m2
+mAdd:: Matrix -> Matrix -> Either String Matrix
+mAdd mat1@(Matrix m1) mat2@(Matrix m2)
+    | not sameSize = Left $ getErrMsg 20 "mAdd"
+    | otherwise = Right $ Matrix (mAddInternal m1 m2)
+    where sameSize = (size mat1) == (size mat2)
+          mAddInternal:: (LinkedList Row) -> (LinkedList Row) -> (LinkedList Row)
+          mAddInternal (EmptyList) m = m
+          mAddInternal m (EmptyList) = m
+          mAddInternal m1@(Node d1 t1) m2@(Node d2 t2) = (makeNodeT (sumRow d1 d2) (mAddInternal t1 t2))
+
+---- Subtacts two matrices: m1 + (-1)*m2
+mSub:: Matrix -> Matrix -> Either String Matrix
+mSub m1 m2 = mAdd m1 (scale m2 (-1))
+
+---- Transposes a matrix: swaps the rows with the columns
+transpose:: Matrix -> Matrix
+transpose m = transposeInternal (listToMatrix (LST.transpose (matrixToList m)))
+            where transposeInternal:: Either String Matrix -> Matrix
+                  transposeInternal (Left s) = Util.err s
+                  transposeInternal (Right mat) = mat
+
+---- Computes the trace of matrix m: sum of the diagonal of m
+trace:: Matrix -> Either String Double
+trace mat@(Matrix m)
+    | not (isSquare mat) = Left $ getErrMsg 21 "trace"
+    | otherwise = Right (computeTrace m (0,(snd sizeM)-1))
+    where sizeM = size mat
+          computeTrace:: (LinkedList Row) -> (Int,Int) -> Double
+          computeTrace (EmptyList) _ = 1.0
+          computeTrace m@(Node r rs) (cnt,cols)
+                    | cnt > cols = 1.0
+                    | otherwise = (getMaybeDouble (getData (getNodeRec r (cnt))) 1.0) * (computeTrace rs (cnt+1,cols))
+
+---- Computes the determinant of a Matrix <= 2x2, or the First Row Expansion of a Matrix > 2x2
+determinant:: Matrix -> Either String Double
+determinant (Matrix (EmptyList)) = Left $ getErrMsg 22 "determinant"
+determinant mat@(Matrix m@(Node r@(Node d t) rs))
+    | not (isSquare mat) = Left $ getErrMsg 21 "determinant"
+    | (fst sizeM) == 1 = Right $ (getMaybeDouble (getData (getMaybeRow (getData m))) 1.0)
+    | (fst sizeM) == 2 = Right $ ((getMaybeDouble (getData (getNodeRec r 0)) 1.0) * (getMaybeDouble (getData (getNodeRec (getMaybeRow (getData rs)) 1)) 1.0)) - ((getMaybeDouble (getData (getNodeRec (getMaybeRow (getData rs)) 0)) 1.0) * (getMaybeDouble (getData (getNodeRec r 1)) 1.0))
+    | otherwise = Right $ sum [ e | n <- [0..((snd sizeM)-1)], let e = ((getMaybeDouble (getData (getNodeRec r n)) 1.0) * (coFactor mat (0,n))) ]
+    where sizeM = size mat
+
+---- Returns the `m,n coFactor' of param 1, with param 2 providing (m,n)
+coFactor:: Matrix -> (Int,Int) -> Double
+coFactor (Matrix (EmptyList)) _ = 0
+coFactor mat@(Matrix m@(Node r rs)) (row,col) = (sgn (row+col)) * (getRightDouble minor)
+            where sizeM = size mat
+                  minor = determinant (getRightMatrix (listToMatrix (LST.transpose igColList)))
+                  igRowList = [ (fst rowN) | rowN <- (zip (matrixToList mat) [0..((fst sizeM)-1)]), (snd rowN) /= row ]
+                  igColList = [ (fst rowN) | rowN <- (zip (LST.transpose igRowList) [0..((snd sizeM)-1)]), (snd rowN /= col) ]
+                  sgn:: Int -> Double
+                  sgn x
+                    | odd x = -1.0
+                    | even x = 1.0
+
+---- Returns number of rows
+numRows:: Matrix -> Int
+numRows (Matrix m) = len m
+
+---- Returns number of columns in the first row
+numCols:: Matrix -> Int
+numCols (Matrix (Node d _)) = len d
+
+---- Gets the dimensions of a matrix
+size:: Matrix -> Dim
+size m = (numRows m, numCols m)
+
+---- Checks if matrix m is jagged. in other words, if matrix m
+----   has a varying number of columns per row(thus m is invalid)
+----   True if jagged; False otherwise
+checkJagged:: Matrix -> Bool
+checkJagged (Matrix m) = checkJaggedInternal m
+    where leng = len $ getMaybeRow (getData m)
+          checkJaggedInternal:: (LinkedList Row) -> Bool
+          checkJaggedInternal (EmptyList) = False
+          checkJaggedInternal (Node r rs) = ((len r) /= leng) || (checkJaggedInternal rs)
+
+---- Checks if matrix m is a sqaure matrix(# of rows == # of columns)
+isSquare:: Matrix -> Bool
+isSquare m = (numRows m) == (numCols m)
+
+
+--- **Helper Functions**
+
+---- Scales the data in param 1 by param 2
+scaleRow:: Row -> Double -> Row
+scaleRow (EmptyList) _ = EmptyList
+scaleRow (Node d t) x = makeNodeT (d * x) (scaleRow t x)
+
+---- Sums the data in param 1 with the corresponding data in param 2
+sumRow::Row -> Row -> Row
+sumRow (EmptyList) (EmptyList) = EmptyList
+sumRow r (EmptyList) = r
+sumRow (EmptyList) r = r
+sumRow (Node d1 t1) (Node d2 t2) = makeNodeT (d1+d2) (sumRow t1 t2)
 
 
 --- **Misc Functions**
 
----- Returns the number of components for a given vector
-numComponents:: Vector -> Int
-numComponents (EmptyVec) = 0
-numComponents v@(ZeroVec _) = (numComponents (v))
-numComponents (Vec1D _) = 1
-numComponents (Vec2D _ _) = 2
-numComponents (Vec3D _ _ _) = 3
-numComponents (VecND _ t) = 1 + (numComponents t)
+---- Gets a Row from a `Maybe Row'
+getMaybeRow:: (Maybe Row) -> Row
+getMaybeRow mr = Util.getMaybeVal mr EmptyList
 
-getValOrErr:: Either String Vector -> Vector
-getValOrErr (Right b) = b
-getValOrErr (Left a) = Util.err a
+---- Gets a Double from a `Maybe Double'
+getMaybeDouble:: (Maybe Double) -> Double -> Double
+getMaybeDouble md x = Util.getMaybeVal md x
+
+---- Gets a Matrix from a `Either String Matrix'
+getRightMatrix:: Either String Matrix -> Matrix
+getRightMatrix esm = Util.getRightVal esm
+
+---- Gets a Double from a `Either String Double'
+getRightDouble:: Either String Double -> Double
+getRightDouble esd = Util.getRightVal esd
+
+---- Returns an error message from given a error code
+getErrMsg:: Int -> String -> String
+getErrMsg 10 context = (getErrPrefix context) ++ "Matrix must be of size 1x1 or greater!"
+getErrMsg 11 context = (getErrPrefix context) ++ "Jagged Matrices are invalid!"
+getErrMsg 20 context = (getErrPrefix context) ++ "Cannot Add/Substract Matrices of different dimensions!"
+getErrMsg 21 context = (getErrPrefix context) ++ "Non-Square Matrix Provided"
+getErrMsg 22 context = (getErrPrefix context) ++ "Empty Matrix Provided"
+
+getErrPrefix:: String -> String
+getErrPrefix context = "[Matrix/Matrix.hs: " ++ context ++ "]"
--- Matrix/Util.hs
+++ Matrix/Util.hs
@@ -1,9 +1,17 @@
 -- ***Utilities***
 module Matrix.Util
-( ListMatrix(..)
+(
+{- Type Synomyms -}
+  ListMatrix(..)
+{- Functions -}
+, getMaybeVal
+, getRightVal
 , nyi
 , err
 ) where
+
+
+--- **Imports**
 
 
 --- **Type Synomyms**
@@ -13,6 +21,16 @@ type ListMatrix a = [[a]]
 
 
 --- **Utility Functions**
+
+---- Gets the Value, v, out of a `Just v' or param 2 if param 1 is of type `Nothing'
+getMaybeVal:: (Maybe a) -> a -> a
+getMaybeVal (Just v) _ = v
+getMaybeVal (Nothing) d = d
+
+---- Gets the Value, v, out of a `Right v' or errors with String, s, from `Left s' 
+getRightVal:: (Either String a) -> a
+getRightVal (Left s) = err s
+getRightVal (Right v) = v
 
 ---- Errors with a NYI message and the given context
 nyi::String -> null
